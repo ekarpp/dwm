0074579df7020e13603b3d3ded316c0ef4514271
ekarpp <eetu.karppinen@aalto.fi>
2021-08-15 20:25:53 +0300

    status app: add load and free mem

--- dwm-status.c
+++ dwm-status.c
@@ -1,12 +1,33 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/sysinfo.h>
 #include <time.h>
 #include <unistd.h>
 #include <X11/Xlib.h>
 
 #define LEN 64
 #define STATUS_LEN 256
+#define LOAD_SCALE 1.0d / (1 << SI_LOAD_SHIFT)
+
+char *get_sysinfo(void)
+{
+    struct sysinfo *info;
+    char *ret = malloc(LEN);
+    ret[0] = '\0';
+
+    if (sysinfo(info) < 0)
+        return ret;
+
+    snprintf(ret, LEN - 1, "M: %.2f GiB L: %.2f %.2f %.2f",
+             (double) info->freeram / (1 << 30),
+             info->loads[0] * LOAD_SCALE,
+             info->loads[1] * LOAD_SCALE,
+             info->loads[2] * LOAD_SCALE
+        );
+
+    return ret;
+}
 
 char *get_time(void)
 {
@@ -42,21 +63,24 @@ int main(int argc, char *argv[])
     win = DefaultRootWindow(dpy);
 
     char *time;
+    char *sys;
     char status[STATUS_LEN];
 
     while (1)
     {
         time = get_time();
-        snprintf(status, STATUS_LEN - 1, "%s", time);
+        sys = get_sysinfo();
+        snprintf(status, STATUS_LEN - 1, "%s %s", sys, time);
 
         XStoreName(dpy, win, status);
         XFlush(dpy);
 
         free(time);
+        free(sys);
         sleep(60);
     }
 
     return 0;
 }
 
-// load, temp (CPU, GPU), clock, time, RAM, VOLUME
+// temp (CPU, GPU), VOLUME
