04e84dbca50585f1281cf08b301acdae8bca2475
ekarpp <eetu.karppinen@aalto.fi>
2021-08-15 16:12:44 +0300

    refactor status app

--- dwm-status.c
+++ dwm-status.c
@@ -5,36 +5,58 @@
 #include <unistd.h>
 #include <X11/Xlib.h>
 
-int main(int argc, char * argv[])
+#define LEN 64
+#define STATUS_LEN 256
+
+char *get_time(void)
 {
-        Display * dpy = NULL;
-        Window win = 0;
-        size_t length = 0;
-        ssize_t bytes_read = 0;
-        char * input = NULL;
+    time_t rawtime;
+    struct tm *timeinfo;
+    char *ret = malloc(LEN);
+    ret[0] = '\0';
 
-        dpy = XOpenDisplay(getenv("DISPLAY"));
-        if (dpy == NULL)
-        {
-                fprintf(stderr, "Can't open display, exiting.\n");
-                exit(1);
-        }
-        win = DefaultRootWindow(dpy);
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    if (timeinfo == NULL)
+        return ret;
 
-        time_t rawtime;
-        struct tm * timeinfo;
-        char text[128];
-        while (1)
-        {
-            time(&rawtime);
-            timeinfo = localtime(&rawtime);
-            strftime(text, 500, "%a %d %b %R", timeinfo);
-            XStoreName(dpy, win, text);
-            XFlush(dpy);
-            sleep(60);
-        }
+    strftime(ret, LEN - 1, "%a %d %b %R", timeinfo);
 
-        return 0;
+    return ret;
+}
+
+int main(int argc, char *argv[])
+{
+    Display *dpy = NULL;
+    Window win = 0;
+    size_t length = 0;
+    ssize_t bytes_read = 0;
+    char *input = NULL;
+
+    dpy = XOpenDisplay(getenv("DISPLAY"));
+    if (dpy == NULL)
+    {
+        fprintf(stderr, "Can't open display, exiting.\n");
+        exit(1);
+    }
+    win = DefaultRootWindow(dpy);
+
+    char *time;
+    char status[STATUS_LEN];
+
+    while (1)
+    {
+        time = get_time();
+        snprintf(status, STATUS_LEN - 1, "%s", time);
+
+        XStoreName(dpy, win, status);
+        XFlush(dpy);
+
+        free(time);
+        sleep(60);
+    }
+
+    return 0;
 }
 
 // load, temp (CPU, GPU), clock, time, RAM, VOLUME
